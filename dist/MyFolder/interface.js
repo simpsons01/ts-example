7273406e009a86a6d551509b972f65891e0cf403
simpsons01 <ilikekobe01@gmail.com>
2021-05-03 23:38:16 +0800

    init

--- dist/MyFolder/interface.js
+++
@@ -1,8 +0,0 @@
-var animalPrint = /** @class */ (function () {
-    function animalPrint(property1) {
-        this.property1 = property1;
-    }
-    return animalPrint;
-}());
-new animalPrint({ attack: 2 });
-//# sourceMappingURL=interface.js.map
--- dist/MyFolder/namespace.js
+++
@@ -1,5 +0,0 @@
-var Verson_1;
-(function (Verson_1) {
-    Verson_1.version = "1.1";
-})(Verson_1 || (Verson_1 = {}));
-//# sourceMappingURL=namespace.js.map
