0191603fc0d9ca6ceb7875901ad754a8bd1d8f77
simpsons01 <ilikekobe01@gmail.com>
2021-05-14 02:26:37 +0800

    add intersetion and union

--- src/2_interface/class3_extend.ts
+++ src/2_interface/class3_extend.ts
@@ -12,7 +12,32 @@ const customExtendObj: extendInterface = {
   extendProperty: "i am extend property"
 }
 
-// 2. interface可以被class拓展，接下來implements，只能是該類的subClass，不過使用場景？？？
+// 2. 也可以使用union(聯合型別) intersection(交集型別)
+interface  contractA{
+  age: number
+}
+
+interface  contractB {
+  name: string
+}
+
+type useContractAOrB = contractA | contractB
+type useContractAAndB = contractA & contractB
+
+const contractOne: useContractAOrB = {
+  age: 24
+}
+
+const contractSecond: useContractAOrB = {
+  name: "mike"
+}
+
+const contractThird: useContractAAndB = {
+  age: 15,
+  name: "leo"
+}
+
+// 3. interface可以被class拓展，接下來implements，只能是該類的subClass，不過使用場景？？？
 class baseClass {
   private priveateProperty: boolean
   constructor(val) {
