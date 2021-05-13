dd0431c293c3478bfa5be4f465783a87e58fc698
simpsosns01 <ilikekobe01@gmail.com>
2021-05-13 13:54:36 +0800

    add abstract vs interface

--- src/3_class/class4_modifier copy.ts
+++ src/3_class/class4_modifier copy.ts
@@ -12,7 +12,18 @@ class dummySubBase extends dummyBase {
   }
 }
 
-// 2. 使用getter，未使用setter，該屬性會直接被推斷為read-only屬性
+// 2.設定readonly的property，只能在constructor或是一開始就給定值
+class dummyReadOnlyErrorExample {
+  readonly readonlyProperty1: number = 1
+  readonly readonlyProperty2: number
+  constructor(num) {
+    this.readonlyProperty1 = 2
+    this.readonlyProperty2 = num
+  }
+}
+
+
+// 3. 使用getter，未使用setter，該屬性會直接被推斷為read-only屬性
 class Account {
   _money: number = 0
   get money() {
@@ -24,8 +35,7 @@ class Account {
 }
 
 const ray = new Account()
-ray.money = 100
-// 打開 setter試看看.....
+ray.money = 100 // 打開 setter試看看.....
 
 
 
