1808d3d4bdefb6a6f09dd6c773a0c257817f5ba4
simpsosns01 <ilikekobe01@gmail.com>
2021-05-14 10:23:42 +0800

    add constructor signature example

--- src/2_interface/class5_static_vs_instance.ts
+++ src/2_interface/class5_static_vs_instance.ts
@@ -1,33 +1,35 @@
 // static property vs instance property
 class a {
-  b: number 
+  b: number;
   constructor(arg: number) {
-    this.b = arg
+    this.b = arg;
   }
-  c() {
-
-  }
-  static d = 2
-  static e = 3
-}
-
-class b {
-  arg
-  arg2
-  constructor(arg, arg2) {
-    this.arg = arg
-    this.arg2 = arg2
-  }
+  c() {}
+  static d = 2;
+  static e = 3;
 }
 
 // 以a類別來說，constructor, d, e被ts歸類在static property, b, c被歸類在實體屬性
+//參考： https://stackoverflow.com/questions/58399613/what-is-exactly-the-static-side-and-the-instance-side-in-typescript
+// https://stackoverflow.com/questions/13407036/how-does-interfaces-with-construct-signatures-work
 
-interface e {
-  new(a: number): a
+//
+interface dummyClass {
+  property1: number;
+}
+interface dummyConstructorInterface {
+  new (property1: number): dummyClass;
 }
 
-const z: e = a
+class dummyA implements dummyClass {
+  property1: number;
+  constructor(arg) {
+    this.property1 = arg;
+  }
+}
 
-const z2: e = b 
+function createDummy(dummy: dummyConstructorInterface, num): dummyClass {
+  return new dummy(num);
+}
 
-//參考： https://stackoverflow.com/questions/58399613/what-is-exactly-the-static-side-and-the-instance-side-in-typescript
+createDummy(dummyA, 3)
