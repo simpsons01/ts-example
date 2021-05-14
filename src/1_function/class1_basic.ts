fba0d19c8d74d93fbefe58ab7a84202e7773ef42
simpsosns01 <ilikekobe01@gmail.com>
2021-05-14 16:50:21 +0800

    fix

--- src/1_function/class1_basic.ts
+++ src/1_function/class1_basic.ts
@@ -1,16 +1,16 @@
 // 1. 設置一個基本的函式
-function myAdd(a: number, b: number): number {
-  return a + b;
-}
+// function myAdd(a: number, b: number): number {
+//   return a + b;
+// }
 
-myAdd(1, 2);
+// myAdd(1, 2);
 
 
 // 2. 可選參數用法
 type add = (a: number, b?: number) => number;
 
 let myAdd: add 
-myAdd  = function(a, b = 1){
+myAdd  = function(a, b = 2){
   return a + b
 }
 
@@ -19,6 +19,6 @@ myAdd(1, 2);
 
 
 // 3. 可選參數不能放置在必需參數前面
-function myAdd(a?: number, b: number): number {
-  return a + b;
-}
+// function myAdd(a?: number, b: number): number {
+//   return a + b;
+// }
