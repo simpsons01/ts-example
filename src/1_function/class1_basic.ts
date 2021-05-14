9f2c8bdea1ecf30689c20d659a1d4c45384bd959
simpsosns01 <ilikekobe01@gmail.com>
2021-05-14 16:47:47 +0800

    add func basic

--- src/1_function/class1_basic.ts
+++ src/1_function/class1_basic.ts
@@ -1,15 +1,24 @@
 // 1. 設置一個基本的函式
-type add = (a: number, b: number, c?: number) => number;
-
-function myAdd(a, b, c = 0): add {
-  if (c) {
-    return a + b + c;
-  } else {
-    return a + b;
-  }
+function myAdd(a: number, b: number): number {
+  return a + b;
 }
 
 myAdd(1, 2);
-myAdd(1, 2, 3); //
-myAdd(1, 2, 3, 4);
 
+
+// 2. 可選參數用法
+type add = (a: number, b?: number) => number;
+
+let myAdd: add 
+myAdd  = function(a, b = 1){
+  return a + b
+}
+
+myAdd(1); 
+myAdd(1, 2);
+
+
+// 3. 可選參數不能放置在必需參數前面
+function myAdd(a?: number, b: number): number {
+  return a + b;
+}
